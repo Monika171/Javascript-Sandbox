769d95f9728a2ce0092c1ea995fababffa827ca7
Monika171 <monikarabha171@gmail.com>
2021-06-16 22:07:19 +0530

    FetchAPI

---
+++ ArrowFunctions/app.js
@@ -0,0 +1,27 @@
+// const sayHello = function() {
+//     console.log('Hello');
+// }
+
+// const sayHello = () => 'Hello';
+
+// Returning an object literal
+// const sayHello = () => ({msg: 'Hello'});
+
+// console.log(sayHello());
+
+// Single Parameter
+//  const sayHello = name => console.log(`Hello ${name}`);
+// sayHello('Miranda');
+
+// Multiple Parameters
+// const sayHello = (firstName, lastName) => console.log(`Hello ${firstName} ${lastName}`);
+
+// sayHello('Ole', 'Summ');
+
+const users = ['Nathan', 'John', 'William'];
+
+const nameLengths = users.map(name => name.length);
+
+console.log(nameLengths);
+
+
