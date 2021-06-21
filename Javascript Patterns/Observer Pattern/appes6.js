108f4036442748607525ed8cbf088e2b6095769e
Monika171 <monikarabha171@gmail.com>
2021-06-21 18:18:32 +0530

    Observer Pattern

--- Javascript Patterns/Observer Pattern/appes6.js
+++ Javascript Patterns/Observer Pattern/appes6.js
@@ -2,17 +2,18 @@
 // so gives us a really nice subscription model
 // can to used to notify DOM of certain elements to be updated
 
-function EventObserver() {
-    this.observers = [];
-}
+class EventObserver {
 
-EventObserver.prototype = {
-    subscribe: function(fn) {
+    constructor() {
+        this.observers = [];
+    }
+
+    subscribe(fn) {
         this.observers.push(fn);
         console.log(`You are now subscribed to ${fn.name}`);
-    },
+    }
 
-    unsubscribe: function(fn) {
+    unsubscribe(fn) {
         // Filter out from the list whatever matches the callback function. If there is no match, the callback gets to stay on the list. The filter returns a new list and reassigns the list of observers.
 
         this.observers = this.observers.filter(function(item){
@@ -22,14 +23,15 @@ EventObserver.prototype = {
         });
 
         console.log(`You are now unsubscribed from ${fn.name}`);
-    },
+    }
 
-    fire: function() {
+    fire() {
         this.observers.forEach(function(item) {
             item.call();
-        })
+        });
     }
 }
+
 
 const click = new EventObserver();
 
